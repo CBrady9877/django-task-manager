ae349d76ea28147331c560251695017f3323e738
CBrady9877 <bradycorey98@gmail.com>
2025-02-26 07:15:10 -0500

    fix

--- taskManager/managerapp/views.py
+++ taskManager/managerapp/views.py
@@ -4,9 +4,3 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello Worl!. You are at the managerapp index")
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello Worl!. You are at the managerapp index")
