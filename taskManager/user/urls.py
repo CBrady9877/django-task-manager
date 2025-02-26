54cd7a019c7c6ef615165d3451012a92e22a908e
CBrady9877 <bradycorey98@gmail.com>
2025-02-26 12:19:51 -0500

    Login/Logout seperate user interactons

--- taskManager/user/urls.py
+++ taskManager/user/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("user/", views.user_view, name="user"),
+    path("register/", views.register, name="register"),
+    path("login/", views.login_user, name="login"),
+    path("logout/", views.login_user, name="logout"),
 ]
--- taskManager/user/views.py
+++ taskManager/user/views.py
@@ -1,7 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+from .forms import UserRegisterForm
+
 
 # Create your views here.
 def user_view(request):
     return HttpResponse("User page")
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("task_list")
+    else:
+        form = UserRegisterForm()
+    return render(request, "user/register.html", {"form":form})
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("task_list")
+        else:
+            messages.error(request, "Invalid username or password")
+    return render(request, "user/login.html")
+
+def logout_user(request):
+    logout(request)
+    return redirect("login")
