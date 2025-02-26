cc4a523bffaf00f69bcafd0217ef5d3fda5cfc4f
CBrady9877 <bradycorey98@gmail.com>
2025-02-26 07:33:45 -0500

    created basic views

--- taskManager/managerapp/urls.py
+++ taskManager/managerapp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<int:task_id>/", views.detail, name="detail"),
+    path("<int:task_id>/results/", views.results, name="results"),
+    path("<int:task_id>/results/complete/", views.complete, name="complete"),
 ]
--- taskManager/managerapp/views.py
+++ taskManager/managerapp/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello Worl!. You are at the managerapp index")
+
+def detail(request, task_id):
+    return HttpResponse("You're looking at task %s." % task_id)
+   
+def results(request, task_id):
+    response = "You're looking at a list of task %s."
+    return HttpResponse(response % task_id)
+
+def complete(request, task_id):
+    return HttpResponse("You're completing task %s." % task_id)
